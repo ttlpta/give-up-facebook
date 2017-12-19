5084f46b126b90de148d339210ad29c0404e99be
AnhPT <anh.pham.tuan@solution.com.vn>
2017-12-19 23:15:16 +0700

    Add timepicker

--- background.js
+++ background.js
@@ -1,9 +1,20 @@
-console.log('aaa');
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-    // No tabs or host permissions needed!
-    console.log('Turning ' + tab.url + ' red!');
-    chrome.tabs.executeScript({
-      code: 'document.body.style.backgroundColor="red"'
-    });
-  });
+chrome.webRequest.onBeforeRequest.addListener(
+  function(details) {
+    var currentDate = new Date();
+    var hour = currentDate.getHours();
+    var min = currentDate.getMinutes();
+    var second = currentDate.getSeconds();
+    var currentTime = hour + ':' + min + ':' + second;
+    var start = '21:00:00';
+    var end = '24:00:00';
+
+    return {cancel: start < currentTime && currentTime < end && details.url.indexOf("bongda") != -1};
+  },
+  {urls: ["<all_urls>"]},
+  ["blocking"]);
+
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//     chrome.tabs.executeScript({
+//       code: 'document.body.style.backgroundColor="red"'
+//     });
+//   });
--- popup.js
+++ popup.js
@@ -1,9 +1,10 @@
+$(document).ready(function() {
+  $('#start').timepicker();
+  $('#end').timepicker();
+})
 document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('checkPage');
-    console.log(window.location.href);
-    checkPageButton.addEventListener('click', function() {
-      chrome.tabs.executeScript(null,
-        {code:"alert('aaaa')"});
-      window.close();
-    }, false);
+    // var checkPageButton = document.getElementById('checkPage');
+    // checkPageButton.addEventListener('click', function() {
+    //   window.close();
+    // }, false);
   }, false);
